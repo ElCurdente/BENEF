521ddc24666d4b715992d113f06fddec28e8f455
TotoNezo <antoine.droyer@gmail.com>
2021-10-07 11:58:22 +0200

    Create FormConnexion.js

---
+++ src/FormConnexion.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Form.css';
+
+
+const FormConnexion = () => {
+    return (
+        <div>
+            <h1>Page de Connexion</h1>
+        </div>
+    )
+}
+
+export default FormConnexion
