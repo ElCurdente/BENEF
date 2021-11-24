8c46122973c104f44ea74c2bd046b5298e011fd1
TotoNezo <antoine.droyer@gmail.com>
2021-11-24 09:51:23 +0100

    yes

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,13 @@
 // import React from 'react':
 import FormSignup from './FormSignup'
 import FormSuccess from './FormSuccess'
+<<<<<<< Updated upstream
 import React, {useState} from "react";
 import Accueil from './Accueil'
+=======
+import React, {useState } from "react";
+import './Form.css'
+>>>>>>> Stashed changes
 
 // import hasAccount as Constants from "./useForm"
 
