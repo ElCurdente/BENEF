6fdd45423a0afbe93361c6d1af8a32f3d3cd8ba3
merwanjdly <digitalprozmc@gmail.com>
2021-12-08 15:30:34 +0100

    aa

---
+++ src/Parametre.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Parametre = () => {
+    
+return (
+       <div className="flex h-screen w-screen relative justify-center items-center pt-5">
+                           <h3 class="font-semibold pt-4 pl-4 black">Paramètres</h3>
+                          
+                        </div>
+    )
+}
+
+
+
+export default Parametre
+
