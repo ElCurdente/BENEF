9436c177c7d43abcb79733dc8e7c5fa7831f0bdf
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2021-12-18 16:02:32 +0100

    Update Parametre.js

--- src/Parametre.js
+++ src/Parametre.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Parametre = () => {
     
 return (
+    
+    
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-white">
                 <div id="infos" className="relative grid grid-cols-2 grid-layout w-95vw px-4">
                     
-                       
+                
                         <h1 className="ml-50 text-2xl font-bold">Paramètres</h1>
                        
                     
@@ -29,7 +31,7 @@ return (
                     <p className="col-span-2 mt-2 text-xl pt-10 font-semibold">Affichage</p>
 
                     <li className='col-span-2 mt-2 list-none'>
-                        <ul>Utilisateurs bloqués<input type="checkbox" class="form-checkbox h-5 w-5 text-green-600 ml-20" unchecked></input></ul> 
+                        <ul>Utilisateurs bloqués<input type="checkbox" class="form-checkbox w-10 h-5 text-green-600 ml-20 rounded-full" unchecked></input></ul> 
                         
                       
                
@@ -40,18 +42,20 @@ return (
 
                 <li className='col-span-1 mt-2 list-none'>
                         <ul>Messages privés
-                            <input type="checkbox" class="form-checkbox h-5 w-5 text-green-600 ml-20" unchecked></input>
+                        <input type="checkbox" class="form-checkbox w-10 h-5 text-green-600 ml-20 rounded-full" unchecked></input>
                         </ul> 
                         <ul>UpVotes sur un post publié
-                            <input type="checkbox" class="form-checkbox h-5 w-5 text-green-600 ml-20" unchecked></input>
+                        <input type="checkbox" class="form-checkbox w-10 h-5 text-green-600 ml-20 rounded-full" unchecked></input>
                         </ul> 
                         <ul>Autres
-                            <input type="checkbox" class="form-checkbox h-5 w-5 text-green-600 ml-20" unchecked></input>
+                            <input type="checkbox" class="form-checkbox w-10 h-5 text-green-600 ml-20 rounded-full" unchecked></input>
+  
                         </ul> 
     
-  
 
 </li>
+
+
 
 
                 </div>
