e5b678f9181b946a1cc6e31fa1859c3ed0bbdbb4
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2022-01-08 23:17:57 +0100

    blocqu

--- src/Bloque.js
+++ src/Bloque.js
@@ -12,7 +12,7 @@ const Parametre = () => {
         <img className="w-100px h-100px bg-transparent dark:bg-gray-650 border-3 border-red-450 dark:border-black rounded-full object-cover " />
         <h1 className="text-center text-2xl font-bold pl-5">Pseudo du boug</h1>
         <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-semibold py-2 px-4 ml-16 rounded-full">
-  Débloqué
+  Débloquer
         </button>
 
 </div>
