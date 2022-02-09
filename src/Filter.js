6eb967fb948743f2acb13e8da8c1c826cf9e06db
ElCurdente <jules.mesnil@gmail.com>
2022-02-09 18:51:54 +0100

    style filtre

--- src/Filter.js
+++ src/Filter.js
@@ -7,242 +7,242 @@ import FilterButton from './FilterButton'
 import Upvote from './Upvote';
 import upvoteBas from './images/icon/upvote.svg';
 import upvoteHaut from './images/icon/upvote2.svg';
+import recherche from './images/icon/icon_recherche.svg';
 
 const Filter = () => {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-    const [input, setInput] = useState("");
-    let filterByFilters;
-    const [filters, setFilters] = useState({
-      filtered:false,
-      category: '',
-      postal: '',
-      filter_by: 'upvote',
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  let filterByFilters;
+  const [filters, setFilters] = useState({
+    filtered: false,
+    category: '',
+    postal: '',
+    filter_by: 'upvote',
   })
 
   const handleReset = () => {
-      setFilters({
-          filtered : false,
-          category:'select',
-          postal:'',
-          filter_by:'',
-      })
+    setFilters({
+      filtered: false,
+      category: 'select',
+      postal: '',
+      filter_by: '',
+    })
   }
 
   const handleChange = e => {
-      const { name, value } = e.target;
-      setFilters({
-        ...filters,
-        [name]: value
-      });
-    };
-  
-    useEffect(() => {
-      fetch("https://benef-app.fr/api-post-render.php")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result.items);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
+    const { name, value } = e.target;
+    setFilters({
+      ...filters,
+      [name]: value
+    });
+  };
 
-    const handleSubmitFiltered = e => {
-      e.preventDefault();
-      setFilters(prevState => ({
-        ...prevState,
-        filtered:true
-      }));
-  }
-
-  function compareUpvote( a, b ) {
-    if ( parseInt(a.upvote) < parseInt(b.upvote) ){
-      return 1;
-    }
-    if ( parseInt(a.upvote) > parseInt(b.upvote) ){
-      return -1;
-    }
-    return 0;
-  }
-
-  function compareDate( a, b ) {
-    if ( parseInt(a.id_post) < parseInt(b.id_post) ){
-      return 1;
-    }
-    if ( parseInt(a.id_post) > parseInt(b.id_post) ){
-      return -1;
-    }
-    return 0;
-  }
-  
-    if (error) {
-      return <div>Erreur : {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Chargement...</div>;
-    } else {
-        const regexp = new RegExp(input, 'i');
-        const regexp_postal = new RegExp(filters.postal, 'i');
-        const regexp_category = new RegExp(filters.category, 'i');
-
-        const filterByName = items.filter(x => regexp.test(x.title));
-        console.log(filters.postal);
-        filterByFilters = items.filter(item => regexp_postal.test(item.postal));
-        const filter100= filterByFilters.filter(item => regexp_category.test(item.category))
-        const filter1000 = filter100.filter(x => regexp.test(x.title));
-        if(filters.filter_by === "upvote"){
-          filterByFilters.sort(compareUpvote);
-        }else if(filters.filter_by === "date"){
-          filterByFilters.sort(compareDate);
+  useEffect(() => {
+    fetch("https://benef-app.fr/api-post-render.php")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.items);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
         }
-        console.log(filterByFilters);
+      )
+  }, [])
 
-      return (
-        <div className="h-screen w-screen bg-white-150 flex justify-center overflow-auto items-center">
-          <button></button>
-          <ul className="h-full">
-            <div className="mt-20 pb-12">
+  const handleSubmitFiltered = e => {
+    e.preventDefault();
+    setFilters(prevState => ({
+      ...prevState,
+      filtered: true
+    }));
+  }
+
+  function compareUpvote(a, b) {
+    if (parseInt(a.upvote) < parseInt(b.upvote)) {
+      return 1;
+    }
+    if (parseInt(a.upvote) > parseInt(b.upvote)) {
+      return -1;
+    }
+    return 0;
+  }
+
+  function compareDate(a, b) {
+    if (parseInt(a.id_post) < parseInt(b.id_post)) {
+      return 1;
+    }
+    if (parseInt(a.id_post) > parseInt(b.id_post)) {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (error) {
+    return <div>Erreur : {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Chargement...</div>;
+  } else {
+    const regexp = new RegExp(input, 'i');
+    const regexp_postal = new RegExp(filters.postal, 'i');
+    const regexp_category = new RegExp(filters.category, 'i');
+
+    const filterByName = items.filter(x => regexp.test(x.title));
+    console.log(filters.postal);
+    filterByFilters = items.filter(item => regexp_postal.test(item.postal));
+    const filter100 = filterByFilters.filter(item => regexp_category.test(item.category))
+    const filter1000 = filter100.filter(x => regexp.test(x.title));
+    if (filters.filter_by === "upvote") {
+      filterByFilters.sort(compareUpvote);
+    } else if (filters.filter_by === "date") {
+      filterByFilters.sort(compareDate);
+    }
+    console.log(filterByFilters);
+
+    return (
+      <div className="h-screen w-screen bg-white-150 flex justify-center overflow-auto items-center">
+        <ul className="h-full">
+          <div className="mt-14 pb-12">
             <form className="post flex flex-col justify-center" onSubmit={(e) => handleSubmitFiltered(e)} id="filter_form">
-    <div className="flex h-100px relative justify-center items-center w-4/5">
-
-    </div>
-    <div className="flex  relative justify-center items-center">
-        <label htmlFor="cat" className="">{filters.category}</label>
-        <select name="category" id="category" onChange={handleChange}>
-            <option value="select">--Please choose an option--</option>
-            <option value="all">Toutes catégories</option>
-            <option value="1">Catégorie 1</option>
-            <option value="2">Catégorie 2</option>
-            <option value="3">Catégorie 3</option>
-            <option value="4">Catégorie 4</option>
-            <option value="5">Catégorie 5</option>
-            <option value="6">Catégorie 6</option>
-        </select>
-        </div>
-
-    <div className="flex  relative justify-center items-center">
-        <label htmlFor="postal" className="">
-        </label>
-        <select name="postal" id="postal" onChange={handleChange}>
-            <option value="select">--Choisissez une région--</option>
-            <option value="all">Tous</option>
-            <option value="75">75</option>
-            <option value="78">78</option>
-            <option value="91">91</option>
-            <option value="92">92</option>
-            <option value="93">93</option>
-            <option value="94">94</option>
-            <option value="95">95</option>
-            <option value="77">77</option>
-        </select>
-    </div>
-
-    <div className="flex  relative justify-center items-center">
-        <label htmlFor="expiration" className="text-black">
-        </label>
-        <input id="expiration"
-            type="date"
-            name="expiration"
-            maxLength="30"
-            placeholder="Date d'expiration" className="appearance-none placeholder-black text-white-150 border-b-2 bg-transparent w-4/5 my-2 h-12 pt-5 text-left focus:outline-none  focus:placeholder-transparent"
-            value={filters.expiration}
-            onChange={handleChange}
-        />
-    </div>
-
-    <div className="flex relative items-center mt-6 ml-3">
-    <h4 className="text-black pl-2">{filters.filter_by}</h4>
-    <label htmlFor="certified" className="text-black pl-2">
-        <input id="filter_by_date"
-            type="radio"
-            name="filter_by"
-            maxLength="30"
-            className=""
-            value="date"
-            onChange={handleChange}
-        />
-        Upvotes
-        </label>
-        </div>
-        <div className="flex relative items-center mt-6 ml-3">
-        <label className="text-black pl-2">
-         <input id="filter_by_upvote"
-            type="radio"
-            name="filter_by"
-            maxLength="30"
-            className=""
-            value="upvote"
-            onChange={handleChange}
-        />
-    Récent
-    </label>
-    </div>
-    <div className="flex justify-end items-center py-5 mr-5">
-        <button className="block w-24 h-9 text-red-450 text-lg font-bold border-2 border-white-0 bg-white-0 hover:bg-red-450 hover:text-white-0 hover:border-white-0 dark:hover:bg-white-150 dark:hover:text-gray-550 active:bg-red-200 dark:bg-white-0 dark:text-black rounded-full transition duration-300 ease-in-out" type="submit">Appliquer</button>
-    </div>
-    <div className="flex justify-end items-center py-5 mr-5">
-        <button onClick={handleReset} className="block  h-9 text-red-450 text-lg font-bold border-2 border-white-0 bg-white-0 hover:bg-red-450 hover:text-white-0 hover:border-white-0 dark:hover:bg-white-150 dark:hover:text-gray-550 active:bg-red-200 dark:bg-white-0 dark:text-black rounded-full transition duration-300 ease-in-out">Réinitialiser</button>
-    </div>
-
-</form>  
-
-
-          <input value={input} onChange={event => setInput(event.target.value)}/>
-          {(filters.filtered ?
-          
-          filter1000.map(item => (
-            <div className="w-92vw h-300px relative bg-red-450 dark:bg-black rounded-lg text-white-0 mb-2">
-              <div className="w-full h-75% relative">
-                <img className="object-cover rounded-t-lg h-full w-full" src={profil} alt="" />
-                <div className="absolute bottom-5 -left-1.5">
+              <div className="flex h-100px relative justify-center items-center w-full">
+                <h1 className="text-center text-2xl font-bold pt-7">Recherche</h1>
+              </div>
+              <div className="flex relative justify-center items-center w-full ">
+                <div className="flex border-b-2 border-black required:w-65% h-8 mt-5 mb-20">
+                  <input value={input} className=" required:w-65% px-6 bg-white-150 placeholder-black focus:outline-none" placeholder="Rechercher..." onChange={event => setInput(event.target.value)} />
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 20 20" className="w-5 h-5 mt-1 mr-3"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
               </div>
-              <div className="w-full h-25%">
-                <li key={item.id_post}  className="mt-1 w-92vw">
-                                    <div>
-                          <div className="bg-white-0 text-black text-xl font-bold flex w-max py-1 rounded-lg">
-                            <span className="px-2 upvote">{item.upvote}</span>
-                          </div>
-                        </div>
-                  <h1 className="text-lg font-semibold mx-2">{item.title}</h1>
-                  <div className="flex mt-2 text-sm w-92vw">
-                    <img src={adresse} className="ml-2 mr-1 w-3.5"></img> {item.address} <div className="absolute right-3">{item.postal}</div>
+              <div className="flex  relative justify-center items-center">
+                <label htmlFor="cat" className="">{filters.category}</label>
+                <select name="category" id="category" onChange={handleChange}>
+                  <option value="select">--Please choose an option--</option>
+                  <option value="all">Toutes catégories</option>
+                  <option value="1">Catégorie 1</option>
+                  <option value="2">Catégorie 2</option>
+                  <option value="3">Catégorie 3</option>
+                  <option value="4">Catégorie 4</option>
+                  <option value="5">Catégorie 5</option>
+                  <option value="6">Catégorie 6</option>
+                </select>
+              </div>
+
+              <div className="flex  relative justify-center items-center">
+                <label htmlFor="postal" className="">
+                </label>
+                <select name="postal" id="postal" onChange={handleChange}>
+                  <option value="select">--Choisissez une région--</option>
+                  <option value="all">Tous</option>
+                  <option value="75">75</option>
+                  <option value="78">78</option>
+                  <option value="91">91</option>
+                  <option value="92">92</option>
+                  <option value="93">93</option>
+                  <option value="94">94</option>
+                  <option value="95">95</option>
+                  <option value="77">77</option>
+                </select>
+              </div>
+
+              <div className="flex  relative justify-center items-center">
+                <label htmlFor="expiration" className="text-black">
+                </label>
+                <input id="expiration"
+                  type="date"
+                  name="expiration"
+                  maxLength="30"
+                  placeholder="Date d'expiration" className="appearance-none placeholder-black text-white-150 border-b-2 bg-transparent w-4/5 my-2 h-12 pt-5 text-left focus:outline-none  focus:placeholder-transparent"
+                  value={filters.expiration}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="flex relative items-center mt-6 ml-3">
+                <h4 className="text-black pl-2">{filters.filter_by}</h4>
+                <label htmlFor="certified" className="text-black pl-2">
+                  <input id="filter_by_date"
+                    type="radio"
+                    name="filter_by"
+                    maxLength="30"
+                    className=""
+                    value="date"
+                    onChange={handleChange}
+                  />
+                  Upvotes
+                </label>
+              </div>
+              <div className="flex relative items-center mt-6 ml-3">
+                <label className="text-black pl-2">
+                  <input id="filter_by_upvote"
+                    type="radio"
+                    name="filter_by"
+                    maxLength="30"
+                    className=""
+                    value="upvote"
+                    onChange={handleChange}
+                  />
+                  Récent
+                </label>
+              </div>
+              <div className="flex justify-end items-center py-5 mr-5">
+                <button className="block w-24 h-9 text-red-450 text-lg font-bold border-2 border-white-0 bg-white-0 hover:bg-red-450 hover:text-white-0 hover:border-white-0 dark:hover:bg-white-150 dark:hover:text-gray-550 active:bg-red-200 dark:bg-white-0 dark:text-black rounded-full transition duration-300 ease-in-out" type="submit">Appliquer</button>
+              </div>
+              <div className="flex justify-end items-center py-5 mr-5">
+                <button onClick={handleReset} className="block  h-9 text-red-450 text-lg font-bold border-2 border-white-0 bg-white-0 hover:bg-red-450 hover:text-white-0 hover:border-white-0 dark:hover:bg-white-150 dark:hover:text-gray-550 active:bg-red-200 dark:bg-white-0 dark:text-black rounded-full transition duration-300 ease-in-out">Réinitialiser</button>
+              </div>
+
+            </form>
+            {(filters.filtered ?
+
+              filter1000.map(item => (
+                <div className="w-92vw h-150px relative flex justify-between items-center bg-red-450 dark:bg-black rounded-lg text-white-0 mb-5 shadow-customm">
+                  <div className="h-full w-35% flex justify-center items-center relative">
+                    <img className="w-90% h-90%  object-cover rounded-lg" src={profil} alt="" />
                   </div>
-                </li>
-              </div>
-            </div>
-          ))
-          :
-          filterByName.map(item => (
-            <div className="w-92vw h-150px relative flex justify-between items-center bg-red-450 dark:bg-black rounded-lg text-white-0 mb-5 shadow-customm">
-              <div className="h-full w-150px flex justify-center items-center relative">
-                <img className="w-90% h-90%  object-cover rounded-lg" src={profil} alt="" />
-              </div>
-              <div className="w-60% justify-self-end flex relative justify-center items-center h-full">
-                <li key={item.id_post}  className="mt-1 w-92vw">
-                          <div className="bg-white-0 text-black text-xl font-bold absolute right-3 top-3 w-max py-1 rounded-lg">
-                            <span className="px-2 upvote">{item.upvote}</span>
-                          </div>
-                  <h1 className="text-lg font-semibold mx-2">{item.title}</h1>
-                  <div className="flex mt-2 text-sm">
-                    <img src={adresse} className="ml-2 mr-1 w-3.5"></img> {item.address} <div className="absolute right-3">{item.postal}</div>
+                  <div className="w-65% justify-self-end flex relative justify-center items-center h-full">
+                    <li key={item.id_post} className="mt-1 w-92vw">
+                      <div className="bg-white-0 text-black text-xl font-bold absolute right-3 top-3 w-max rounded-lg">
+                        <span className="px-2 upvote">{item.upvote}</span>
+                      </div>
+                      <h1 className="text-lg font-semibold mx-2">{item.title}</h1>
+                      <div className="flex mt-2 text-sm">
+                        <img src={adresse} className="ml-2 mr-1 w-3.5"></img> {item.address} <div className="absolute right-3 bottom-3">{item.postal}</div>
+                      </div>
+                    </li>
                   </div>
-                </li>
-              </div>
-            </div>
-          ))
-          )}
-              
-            </div>
-          </ul>
-        </div >  
-      );
-      
-    }};
+                </div>
+              ))
+              :
+              filterByName.map(item => (
+                <div className="w-92vw h-150px relative flex justify-between items-center bg-red-450 dark:bg-black rounded-lg text-white-0 mb-5 shadow-customm">
+                  <div className="h-full w-35% flex justify-center items-center relative">
+                    <img className="w-90% h-90%  object-cover rounded-lg" src={profil} alt="" />
+                  </div>
+                  <div className="w-65% justify-self-end flex relative justify-center items-center h-full">
+                    <li key={item.id_post} className="mt-1 w-92vw">
+                      <div className="bg-white-0 text-black text-xl font-bold absolute right-3 top-3 w-max rounded-lg">
+                        <span className="px-2 upvote">{item.upvote}</span>
+                      </div>
+                      <h1 className="text-lg font-semibold mx-2">{item.title}</h1>
+                      <div className="flex mt-2 text-sm">
+                        <img src={adresse} className="ml-2 mr-1 w-3.5"></img> {item.address} <div className="absolute right-3 bottom-3">{item.postal}</div>
+                      </div>
+                    </li>
+                  </div>
+                </div>
+              ))
+            )}
+
+          </div>
+        </ul>
+      </div >
+    );
+
+  }
+};
 
 export default Filter;
