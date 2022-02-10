b9787b3b1776a9d5c377d190a7c50f10534ad4c3
Bigou <antoine.droyer@gmail.com>
2022-02-10 15:53:32 +0100

    oui

--- src/Filter.js
+++ src/Filter.js
@@ -96,9 +96,9 @@ const Filter = () => {
         const filter100= filterByFilters.filter(item => regexp_category.test(item.category))
         const filter1000 = filter100.filter(x => regexp.test(x.title));
         if(filters.filter_by === "upvote"){
-          filterByFilters.sort(compareUpvote);
+          filter1000.sort(compareUpvote);
         }else if(filters.filter_by === "date"){
-          filterByFilters.sort(compareDate);
+          filter1000.sort(compareDate);
         }
         console.log(filterByFilters);
 
@@ -195,15 +195,9 @@ const Filter = () => {
           <input value={input} onChange={event => setInput(event.target.value)}/>
           {(filters.filtered ?
           
-<<<<<<< HEAD
           filter1000.map(item => (
             <div className="w-92vw h-300px relative bg-red-450 dark:bg-black rounded-lg text-white-0 mb-2">
               <div className="w-full h-75% relative">
-=======
-          filterByFilters.map(item => (
-            <div className="w-92vw h-100px relative bg-red-450 dark:bg-black rounded-lg text-white-0 mb-2">
-              <div className="w-20% h-75% relative">
->>>>>>> 90e96049a5f6a64568b5b60882797db88aeef01a
                 <img className="object-cover rounded-t-lg h-full w-full" src={profil} alt="" />
                 <div className="absolute bottom-5 -left-1.5">
                 </div>
