ffbd4645add5abdc2b9b3563ae071132cef92711
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2022-02-11 11:16:08 +0100

    kgfkgf

--- src/Parametre.js
+++ src/Parametre.js
@@ -68,27 +68,21 @@ const Parametre = () => {
                 </li>
 
 
-                <h2 className="mt-2 text-xl pt-7 font-semibold ">Notifications</h2>
+                <h2 className="mt-2 text-xl pt-7 font-semibold ">À propos</h2>
                 <li className='mt-2 list-none'>
-                    <ul className="w-full flex justify-between items-center font-normal pb-2">Messages privés
-                        <ToggleBtn2 id='2' />
-                    </ul>
-                    <ul className="w-full flex justify-between items-center font-normal pb-2">"UpVotes" sur un post publié
-                        {/* <input type="checkbox" id="toggle_btn_3" class="hidden w-10 h-5 text-green-600 mr-2 rounded-full" checked={checked3}
-                            onChange={() => setChecked3(!checked3)} ></input>
-                        <label for="toggle_btn_3">
-                            <div className="container-toggle-dot flex w-9 h-5 items-center bg-gray-300 rounded-full p-1 mr-2 transform transition duration-300">
-                                <div id="toggle_dot_3" className="toggle-dot-3 w-4 h-4 bg-white-0 rounded-full shadow-md transform transition duration-300"></div>
-                            </div>
-                        </label> */}
-                        <ToggleBtn2 id='3' />
-                    </ul>
-                    <ul className="w-full flex justify-between items-center font-normal">Autres
+                    
+                    <ul className="w-full flex justify-between items-center font-normal pb-2">
+                        <Link to="/Cgu">
+                            <a className="cursor-pointer font-normal">Conditions d'utilisations générales</a>
+                        </Link>
+                        </ul>
+                        
+                    <ul className="w-full flex justify-between items-center font-normal pb-2">
+                    Contact
                     </ul>
 
                 </li>
-                <h2 className="mt-2 p-0 text-base pt-10 font-semibold cursor-pointer ">Aide</h2>
-                <h2 className="mt-2 text-base font-semibold cursor-pointer ">Déconnexion</h2>
+                <h2 className="mt-2  pt-7 text-base font-semibold cursor-pointer ">Déconnexion</h2>
                 <h2 className="mt-2 text-base font-semibold text-red-500 cursor-pointer">Supprimer le compte</h2>
 
             </div>
