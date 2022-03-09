d0b333dea6c3860ab7c95da31cf5d74c1999f4c5
ElCurdente <jules.mesnil@gmail.com>
2022-03-09 16:02:17 +0100

    test loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Form from './Form';
 import './Form.css';
-import Accueil from './Accueil'
+import Accueil from './Accueil';
+import Home from './Home';
 import React, {useState} from "react";
 
 
@@ -10,7 +11,7 @@ function App() {
  
   return (
     <div className=" bg-white-0 dark:bg-gray-550 h-screen overflow-y-hidden transition ease-in-out duration-500">
-      {!localStorage.getItem("isConnected") ? ( <Form/>) : (<Accueil/>)}            
+      {!localStorage.getItem("isConnected") ? ( <Form/>) : (<Home/>)}            
     </div>
   );
 }
