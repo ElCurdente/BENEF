5a16ac739bdaf46f6f6203e366c8e739cc3aa9cf
ElCurdente <jules.mesnil@gmail.com>
2022-03-15 13:43:39 +0100

    pdp post filtre + pdp autres utilisateur profil

--- src/Filter.js
+++ src/Filter.js
@@ -125,6 +125,7 @@ const Filter = ({ searchValue, setSearchValue }) => {
           setModalItem(prevState => ({
             ...prevState,
             user_pseudo: data.username,
+            file: data.image
           }));
         })
         .catch(err => {
@@ -182,7 +183,11 @@ const Filter = ({ searchValue, setSearchValue }) => {
 
                 <h1 className="text-sm xl:text-sm max-w-md mt-4">{modalItem.description}</h1>
 
-                <h1 className="self-end text-sm mt-4"> Posté par <span className="font-semibold">{modalItem.user_pseudo}</span></h1>
+                <div className='flex self-end items-center text-sm max-w-md mt-4'>
+                       Posté par <span className="font-semibold cursor-pointer ml-1 mr-2" onClick={handleModal.bind(modalItem)}>{modalItem.user_pseudo}</span> 
+                        <img className="h-8 w-8 xl:border-2 xl:h-8 xl:w-8 rounded-full xl:rounded-full border-2 border-red-450" src={modalItem.file} alt="image de profil" />
+                      
+                    </div>
 
                 <div className="flex w-full justify-evenly mt-5 mb-10">
                   <button onClick={() => setOpenModal2(false)} className="block px-4 font-semibold py-2 bg-red-450 hover:bg-white-0 hover:text-red-450 hover:border-red-450 border-2 border-red-450 dark:hover:bg-white-150 dark:hover:text-gray-550 active:bg-red-200 dark:bg-white-0 dark:border-black dark:text-black rounded-full transition duration-300 ease-in-out text-white-0" type="submit">Fermer</button>
